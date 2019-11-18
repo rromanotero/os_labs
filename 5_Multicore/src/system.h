bb4726779d7fb341cfe1c80ed97de844efb72fe6
Rafael Otero <A01169587@ad.bcit.ca>
2019-11-18 00:15:30 -0800

    added warning that SYSTEM_DEBUG false is broken

--- 5_Multicore/src/system.h
+++ 5_Multicore/src/system.h
@@ -20,7 +20,13 @@ void system_init(void);
 #define SYSTEM_SCREEN_DEPTH                  32
 
 //Debugging
+
+//IMPORTANT:
+//           T H I S     I S    B R O K E N
+// (if you say FALSE, it will fail on the PI. Works on QEMU though.)
 #define SYSTEM_DEBUG_ON               true    //change to true/false. By default debug info goes to SerialA
+// ================================================
+
 
 //File System
 #define SYSTEM_MAX_FAT_TABLE_SIZE_IN_SECTORS    700 //larger starts to take to long to load, shorter and files are not found
